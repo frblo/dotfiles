065f197b846089173765164bbfee89d32e62d91b
Fredrik Blomqvist <scanmar333@gmail.com>
2026-02-04 12:45:29 +0100

    Update treesitter and telescope

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,24 +1,36 @@
-return { -- Highlight, edit, and navigate code
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    main = "nvim-treesitter.configs", -- Sets main module to use for opts
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    config = function()
-        local ensure_installed = {
-            "bash",
-            "c",
-            "diff",
-            "html",
-            "lua",
-            "luadoc",
-            "markdown",
-            "markdown_inline",
-            "query",
-            "vim",
-            "vimdoc",
-        }
-        local ok, nvim_treesitter = pcall(require, "nvim-treesitter")
-        if not ok then return end
-        nvim_treesitter.install(ensure_installed)
-    end
+return {
+	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		local parsers = {
+			"bash",
+			"rust",
+			"c",
+			"diff",
+			"html",
+			"lua",
+			"luadoc",
+			"markdown",
+			"markdown_inline",
+			"query",
+			"vim",
+			"vimdoc",
+			"svelte",
+			"typescript",
+			"javascript",
+		}
+		require("nvim-treesitter").setup()
+
+		vim.defer_fn(function()
+			require("nvim-treesitter").install(parsers):wait(300000)
+		end, 0)
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = parsers,
+			callback = function()
+				vim.treesitter.start()
+			end,
+		})
+	end,
 }
